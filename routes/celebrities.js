aa74f33227b31488e9e8f4e9b583937b2ad464b4
Monika Woznowska <monike@poczta.onet.eu>
2019-05-11 13:17:39 +0200

    add celebrity form added, make it work

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
-
+const mongoose = require("mongoose")
 const Celebrity = require('../models/celebrity');
 
+router.get('/celebrities/new', (req, res) => {
+  res.render('celebrities/new');
+});
 
-router.get('/celebrities', (req, res) => {
+router.get('/celebrities/', (req, res) => {
   Celebrity.find({}, (err, result) => {
     if (err) { 
       return err;
@@ -16,5 +19,35 @@ router.get('/celebrities', (req, res) => {
   });
 });
 
+router.get('/celebrities/:celebId', (req, res) => {
+  Celebrity.find({_id: mongoose.Types.ObjectId(req.params.celebId)}, (err, result) => {
+    if (err) { 
+      return err;
+    } else {
+      res.render('celebrities/show', {
+          celebrities: result
+        });
+    }
+  });
+});
+
+
+
+router.post('/celebrities', (req, res) => {
+  const newCelebrityObject = {
+    name: req.body.name,
+    occupation: req.body.occupation,
+    catchPhrase: req.body.catchPhrase
+  };
+  const newCelebrity = new Celebrity(newCelebrityObject);
+
+  save(newCelebrity, (err) => {
+    if (err) {
+      res.render('celebrity/new')
+    } else {
+      res.render('celebrities');
+    }
+  });
+});
 
 module.exports = router;
